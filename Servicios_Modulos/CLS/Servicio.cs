358189886139c7fd68aacc7c48f066e534827e2a
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2021-05-10 16:35:25 -0600

    se agrego la clase servicio a CLS en Servicios_Modulos

--- Servicios_Modulos/CLS/Servicio.cs
+++ Servicios_Modulos/CLS/Servicio.cs
@@ -11,5 +11,93 @@ namespace Servicios_Modulos.CLS
 {
     class Servicio
     {
+        int _idservicios;
+        String _nombre;
+        String _valorcontado;
+        String _valorcuota;
+        String _descripcion;
+        String _prima;
+
+        public int Idservicios { get => _idservicios; set => _idservicios = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Valorcontado { get => _valorcontado; set => _valorcontado = value; }
+        public string Valorcuota { get => _valorcuota; set => _valorcuota = value; }
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+        public string Prima { get => _prima; set => _prima = value; }
+
+        public Boolean Guardar()
+        {
+            Boolean Resultado = false;
+            String Sentencia = @"INSERT INTO servicios(nombre,valorcontado,valorcuota,descripcion,prima) 
+            VALUES(' " + this._nombre + " ',' " + this._valorcontado + " ',' " + this._valorcuota + " ',' " + this._descripcion + " ',' " + this._prima + " '); ";
+            Console.WriteLine(Sentencia);
+            try
+            {
+                DataManager.CLS.OperacionBD Operacion = new DataManager.CLS.OperacionBD();
+                if (Operacion.Insertar(Sentencia) > 0)
+                {
+                    Resultado = true;
+
+                }
+                else
+                {
+                    Resultado = false;
+                }
+            }
+            catch
+            {
+                Resultado = false;
+            }
+            return Resultado;
+
+        }
+
+        public Boolean Editar()
+        {
+            Boolean Resultado = false;
+            String Sentencia = @"UPDATE servicios SET nombre=' " + this._nombre + " ' , valorcontado=' " + this._valorcontado + " ' , valorcuota=' " + this._valorcuota + " ' , descripcion=' " + this._descripcion + " ' , prima=' " + this._prima + " ' " +
+                               "WHERE idservicios = " + this._idservicios + ";";
+            Console.WriteLine(Sentencia);
+            try
+            {
+                DataManager.CLS.OperacionBD Operacion = new DataManager.CLS.OperacionBD();
+                if (Operacion.Actualizar(Sentencia) > 0)
+                {
+                    Resultado = true;
+                }
+                else
+                {
+                    Resultado = false;
+                }
+            }
+            catch
+            {
+                Resultado = false;
+            }
+            return Resultado;
+        }
+
+        public Boolean Eliminar(int id)
+        {
+            Boolean Resultado = false;
+            String Sentencia = @"DELETE FROM servicios WHERE idservicios = " + id + ";";
+            try
+            {
+                DataManager.CLS.OperacionBD Operacion = new DataManager.CLS.OperacionBD();
+                if (Operacion.Eliminar(Sentencia) > 0)
+                {
+                    Resultado = true;
+                }
+                else
+                {
+                    Resultado = false;
+                }
+            }
+            catch
+            {
+                Resultado = false;
+            }
+            return Resultado;
+        }
     }
 }
